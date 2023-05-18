6e15dd4a91d8f1468e1a8a099077321d5c9fe483
Kushagra Gupta <kushagra.kingg@live.com>
2023-05-19 04:15:19 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/* Event management applicaation with ability to have unique users through sign up and login
+    and ability to create events and invite other users to the event with specific date for the event
+    as well as dashboard for each user to see the events they are invited to and the events they created
+    and the ability to accept or decline the invitation to the event
+    and the ability to delete the event they created
+    and the ability to see the users that accepted the invitation to the event they created
+    and the ability to see the users that declined the invitation to the event they created
+    and the users are mapped via a unique id that link users table to events table
+*/
+#[tokio::main]
+async fn main() {}
