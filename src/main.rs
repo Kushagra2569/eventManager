45d667282f500846cfba2ea77d92ebfddc16e221
Kushagra Gupta <kushagra.kingg@live.com>
2023-05-19 23:18:27 +0530

    added default route and fallback

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,30 @@
     and the ability to see the users that declined the invitation to the event they created
     and the users are mapped via a unique id that link users table to events table
 */
+use axum::{routing::get, Router};
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/login", get(login))
+        .fallback(fallback_handler);
+
+    axum::Server::bind(&"127.0.0.1:3042".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+/// axum handler for any request that fails to match the router routes.
+/// This implementation returns HTTP status code Not Found (404).
+pub async fn fallback_handler(uri: axum::http::Uri) -> impl axum::response::IntoResponse {
+    (
+        axum::http::StatusCode::NOT_FOUND,
+        format!("No route {}", uri),
+    )
+}
+
+async fn login() -> Result<String, ()> {
+    Ok("logged in".to_string())
+}
